79c8cad12febbe7cf71078f8b146a9501b7d4760
Jason Bagwill <mr.bagwill@gmail.com>
2020-11-24 19:48:16 -0500

    first commit

---
+++ Week2Challenge/Greeter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Challenge
+{
+    class Greeter
+    {
+        public void SayHello()
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+        }
+
+        public string SayFarewell(string name)
+        {
+            string farewell = ($"Goodbye {name}");
+            return farewell;
+        }
+
+        public void TimelyGreeting(DateTime timeOfDay)
+        {
+            if(timeOfDay.Hour < 11)
+            {
+                Console.WriteLine("Good Morning");
+            }
+            else if(timeOfDay.Hour >=11 && timeOfDay.Hour < 15)
+            {
+                Console.WriteLine("Good Afternoon");
+            }
+            else if(timeOfDay.Hour >=15 && timeOfDay.Hour < 22)
+            {
+                Console.WriteLine("Good Evening");
+            }
+            else 
+            {
+                Console.WriteLine("Good Night");
+            }
+        }
+    }
+}
---
+++ Week2Challenge/Week2Challenge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Challenge
+{
+    public class Week2Challenge
+    {
+        static void Main(string[] args)
+        {
+            Greeter greeter = new Greeter();
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            greeter.SayHello();
+            string fareWell = greeter.SayFarewell(name);
+            Console.WriteLine(fareWell);
+
+            DateTime timeOfDay = DateTime.Now;
+            greeter.TimelyGreeting(timeOfDay);
+            Console.ReadLine();
+        }
+    }
+}
